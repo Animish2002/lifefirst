f56e235d535f6762e52c1bb73fe5b9cf57a18b42
Animish2002 <animishchopade123@gmail.com>
2025-09-23 22:12:27 +0530

    add margin to company description in AboutSection for better alignment

--- src/Landing/AboutUs.tsx
+++ src/Landing/AboutUs.tsx
@@ -99,7 +99,7 @@ const AboutSection = () => {
                     transition={{ duration: 0.5 }}
                   />
 
-                  <span>Water, Wastewater & Sanitation Solution Company</span>
+                  <span className="mx-auto">Water, Wastewater & Sanitation Solution Company</span>
                 </div>
 
                 {/* Text */}
