1fbec418bf38d0751d39d83fb093a3c72cab2935
Vrushabh S Kulkarni <vrushabhskulkarni@gmail.com>
2025-11-14 04:18:15 +0530

    ts bug fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
           Page Not Found
         </h2>
         <p className="text-gray-300 mb-8">
-          The page you're looking for doesn't exist, was moved, or is temporarily unavailable.
+          The page you&apos;re looking for doesn&apos;t exist, was moved, or is temporarily unavailable.
         </p>
 
         <Link
